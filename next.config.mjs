a0114ab2c75c9af7a77da6acb5a1ea4b0ac30ce8
novice2025sys <novice937@gmail.com>
2025-05-18 19:03:07 -0300

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: ['api.nitropay.com.br', 'qrcode.nitropay.com.br'],
+    unoptimized: true,
+  },
+  env: {
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000',
+  },
+};
+
+export default nextConfig;
